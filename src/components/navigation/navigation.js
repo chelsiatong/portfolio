e4c0b393b7bf7985374366dd03dc4375fe15ca93
chelsia tong <chelsiatong@gmail.com>
2022-08-16 18:33:37 -0400

    styled and made header, need to add routes

--- src/components/navigation/navigation.js
+++ src/components/navigation/navigation.js
@@ -2,8 +2,16 @@ import './navigation.scss';
 
 function Navigation() {
     return (
-        <div>
-            <h1>nav</h1>
+        <div className="sidebar" >
+            <div className="sidebar__container" >
+                <h1 className="sidebar__initial" >CT</h1>
+                <div className="sidebar__header" >
+                    <h3 className="sidebar__nav">home</h3>
+                    <h3 className="sidebar__nav" >about</h3>
+                    <h3 className="sidebar__nav" >projects</h3>
+                    <h3 className="sidebar__nav" >contact</h3>
+                </div>
+            </div>
         </div>
     );
 }
