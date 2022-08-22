a633a7b22e95f736d6d703771151a688933b02e7
chelsia tong <chelsiatong@gmail.com>
2022-08-22 16:16:48 -0400

    created new branch for project section of portfolio

---
+++ src/components/projects/projects.js
@@ -0,0 +1,13 @@
+import './projects.scss';
+
+
+function Projects() {
+    return (
+        <section className="projects" >
+            <h1>hi</h1>
+        </section>
+
+    );
+}
+
+export default Projects;
