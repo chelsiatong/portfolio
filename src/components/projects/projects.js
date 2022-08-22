44ead09c2f726d93dc5197b640ea2ba7249c7d50
chelsia tong <chelsiatong@gmail.com>
2022-08-22 17:33:43 -0400

    added all projects to section

--- src/components/projects/projects.js
+++ src/components/projects/projects.js
@@ -2,6 +2,9 @@ import './projects.scss';
 import Forest from '../../assets/images/forest-mountain.jpeg';
 import Arrow from '../../assets/images/arrow.png';
 import Checkit from '../../assets/images/checkit.jpeg';
+import Moods from '../../assets/images/googlemoods.jpeg';
+import Instagram from '../../assets/images/instagram.jpeg';
+import Office from '../../assets/images/office.jpeg';
 
 function Projects() {
     return (
@@ -13,10 +16,25 @@ function Projects() {
             <img className="projects__head--picture" src={Forest} alt="forest"></img>
             <img className="projects__head--arrow" src={Arrow} alt="arrow"></img>
             <div className="projects__all" >
-                <div className="projects__all--checkit">
+                <div className="projects__all--container">
                     <h2>check-it</h2>
-                    <img className="projects__all--checkit__image" src={Checkit} alt="check-it"></img>
+                    <img className="projects__all--container__checkit" src={Checkit} alt="check-it"></img>
                     <a href="https://check-it-app-ext.netlify.app/"><button>view</button></a>
+                </div>
+                <div className="projects__all--container" >
+                    <h2>google moods</h2>
+                    <img className="projects__all--container__googlemoods" src={Moods} alt="google-moods"></img>
+                    <a href="https://google-moods.netlify.app/"><button>view</button></a>
+                </div>
+                <div className="projects__all--container" >
+                    <h2>instagram clone</h2>
+                    <img className="projects__all--container__instagram" src={Instagram} alt="instagram-clone"></img>
+                    <a href="https://blooming-woodland-37579.herokuapp.com/"><button>view</button></a>
+                </div>
+                <div className="projects__all--container" >
+                    <h2>he said, she said</h2>
+                    <img className="projects__all--container__office" src={Office} alt="the-office"></img>
+                    <a href="https://theoffice-he-said-she-said.netlify.app/"><button>view</button></a>
                 </div>
 
             </div>
