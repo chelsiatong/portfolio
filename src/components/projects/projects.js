49b5d0e6f1172a34868abc9626bcc5857dda9cfd
chelsia tong <chelsiatong@gmail.com>
2022-08-30 16:44:47 -0400

    styled and made responsive

--- src/components/projects/projects.js
+++ src/components/projects/projects.js
@@ -10,19 +10,6 @@ class Projects extends Component {
     render() {
         return (
             <section className="projects" >
-                {/* <div className="projects__section" >
-                    <div className="projects__head" >
-                        <h1 className="projects__head--title" >projects</h1>
-                        <p className="projects__head--description" >add description</p>
-                        <ul className="projects__nav">
-                            <li><Link to="check" spy={true} smooth={true}></Link>check-it</li>
-                            <li className="projects__nav--edit"><Link to="google" spy={true} smooth={true}></Link>google moods</li>
-                            <li className="projects__nav--edit"><Link to="clone" spy={true} smooth={true}></Link>instagram clone</li>
-                            <li className="projects__nav--edit"><Link to="office" spy={true} smooth={true}></Link>he said, she said</li>
-                        </ul>
-                    </div>
-                    <img className="projects__head--picture" src={Forest} alt="forest"></img>
-                </div> */}
                 <img className="projects__head--arrow" src={Arrow} alt="arrow"></img>
                 <div className="projects__all" >
                     <div className="projects__all--container" id="check">
