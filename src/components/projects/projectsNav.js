46ff5236ce775784917a27782f453c4cc16d958f
chelsia tong <chelsiatong@gmail.com>
2022-08-30 13:13:08 -0400

    styled and got react scroll to work

--- src/components/projects/projectsNav.js
+++ src/components/projects/projectsNav.js
@@ -12,10 +12,10 @@ class ProjectsNav extends Component {
                         <h1 className="projects__head--title" >projects</h1>
                         <p className="projects__head--description" >add description</p>
                         <ul className="projects__nav">
-                            <li><Link to="check" spy={true} smooth={true}></Link>check-it</li>
-                            <li className="projects__nav--edit"><Link to="google" spy={true} smooth={true}></Link>google moods</li>
-                            <li className="projects__nav--edit"><Link to="clone" spy={true} smooth={true}></Link>instagram clone</li>
-                            <li className="projects__nav--edit"><Link to="office" spy={true} smooth={true}></Link>he said, she said</li>
+                            <li><Link to="check" spy={true} smooth={true} offset={-20} >check-it</Link></li>
+                            <li className="projects__nav--edit"><Link className="projects__nav--edit" to="google" spy={true} smooth={true} offset={-20}>google moods</Link></li>
+                            <li className="projects__nav--edit"><Link to="clone" spy={true} smooth={true} offset={-20}>instagram clone</Link></li>
+                            <li className="projects__nav--edit"><Link to="office" spy={true} smooth={true} offset={-20}>he said, she said</Link></li>
                         </ul>
                     </div>
                     <img className="projects__head--picture" src={Forest} alt="forest"></img>
