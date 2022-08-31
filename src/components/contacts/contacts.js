1807c1d1ddd5e28dc66279e3591e63b4efe674a2
chelsia tong <chelsiatong@gmail.com>
2022-08-31 15:26:22 -0400

    styling contacts page of portfolio, making it responsive

--- src/components/contacts/contacts.js
+++ src/components/contacts/contacts.js
@@ -1,11 +1,17 @@
 import './contacts.scss';
+import ContactPic from '../../assets/images/contact-photo.jpeg';
 
 
 function Contacts() {
     return (
-        <section className="contacts" >
-            <h1>contacts</h1>
-            <p>add contact section here</p>
+        <section className="contacts">
+            <img className="contacts__mountain-picture" src={ContactPic} alt="Contact-mountain-picture" ></img>
+            <div className="contacts__words">
+                <h1 className="contacts__background" >contact me<br></br>contact me<br></br>contact me<br></br>contact me<br></br>contact me<br></br>contact me<br></br>contact me<br></br>contact me<br></br>contact me<br></br>contact me<br></br>contact me<br></br>contact me<br></br>contact me </h1>
+                <h1 className="contacts__background" >contact me<br></br>contact me<br></br>contact me<br></br>contact me<br></br>contact me<br></br>contact me<br></br>contact me<br></br>contact me<br></br>contact me<br></br>contact me<br></br>contact me<br></br>contact me<br></br>contact me </h1>
+                <h1 className="contacts__background--add" >contact me<br></br>contact me<br></br>contact me<br></br>contact me<br></br>contact me<br></br>contact me<br></br>contact me<br></br>contact me<br></br>contact me<br></br>contact me<br></br>contact me<br></br>contact me<br></br>contact me </h1>
+                <h1 className="contacts__background--add__two" >contact me<br></br>contact me<br></br>contact me<br></br>contact me<br></br>contact me<br></br>contact me<br></br>contact me<br></br>contact me<br></br>contact me<br></br>contact me<br></br>contact me<br></br>contact me<br></br>contact me </h1>
+            </div>
         </section>
 
     );
