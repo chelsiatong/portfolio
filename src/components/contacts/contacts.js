ddecfa3cff5a06555608e97b2ebb18db307cb501
chelsia tong <chelsiatong@gmail.com>
2022-09-02 09:03:35 -0400

    adding contact form for portfolio

--- src/components/contacts/contacts.js
+++ src/components/contacts/contacts.js
@@ -4,17 +4,18 @@ import ContactPic from '../../assets/images/contact-photo.jpeg';
 
 function Contacts() {
     return (
-        <section>
-            <div className="contacts">
+        <section className="contacts" >
+            <div className="contacts__style">
                 <img className="contacts__mountain-picture" src={ContactPic} alt="Contact-mountain-picture" ></img>
                 <div className="contacts__words">
                     <h1 className="contacts__background" >contact me<br></br>contact me<br></br>contact me<br></br>contact me<br></br>contact me<br></br>contact me<br></br>contact me<br></br>contact me<br></br>contact me<br></br>contact me<br></br>contact me<br></br>contact me<br></br>contact me </h1>
                     <h1 className="contacts__background" >contact me<br></br>contact me<br></br>contact me<br></br>contact me<br></br>contact me<br></br>contact me<br></br>contact me<br></br>contact me<br></br>contact me<br></br>contact me<br></br>contact me<br></br>contact me<br></br>contact me </h1>
                     <h1 className="contacts__background--add" >contact me<br></br>contact me<br></br>contact me<br></br>contact me<br></br>contact me<br></br>contact me<br></br>contact me<br></br>contact me<br></br>contact me<br></br>contact me<br></br>contact me<br></br>contact me<br></br>contact me </h1>
-                    <h1 className="contacts__background--add__two" >contact me<br></br>contact me<br></br>contact me<br></br>contact me<br></br>contact me<br></br>contact me<br></br>contact me<br></br>contact me<br></br>contact me<br></br>contact me<br></br>contact me<br></br>contact me<br></br>contact me </h1>
                 </div>
             </div>
-            <h2>stay in touch.</h2>
+            <div className="contacts__section" >
+                <h2>stay in touch.</h2>
+            </div>
         </section>
 
     );
