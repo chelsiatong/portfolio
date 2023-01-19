cb83ae465b7f7b75bc833912dde0cf5d3b42fcd1
chelsia tong <chelsiatong@gmail.com>
2023-01-19 12:42:29 -0500

    cghanged portfolio homepage to add ui ux and changed year

--- src/pages/homepage/homepage.js
+++ src/pages/homepage/homepage.js
@@ -7,9 +7,11 @@ function Homepage() {
             <img className="home__image" src={Mountain} alt="main-mountain"></img>
             <div className="home__info" >
                 <h1 className="home__portfolio" >portfolio</h1>
-                <h3 className="home__year">2022</h3>
+                <h3 className="home__year">2023</h3>
                 <h1 className="home__name">chelsia tong</h1>
                 <h2 className="home__position">front-end <br></br> web developer</h2>
+                <h2 className="home__position--ui">UI/ UX <br></br> designer</h2>
+
             </div>
         </section>
     );
