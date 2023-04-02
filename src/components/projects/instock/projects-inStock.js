25b0a634c26e2f93bf2d473988853ceecfc1bb10
chelsia tong <chelsiatong@gmail.com>
2023-04-02 11:35:04 -0400

    added stock images for instock project

--- src/components/projects/instock/projects-inStock.js
+++ src/components/projects/instock/projects-inStock.js
@@ -1,5 +1,12 @@
 import React, { Component } from 'react';
 import './inStock.scss';
+import warehouse from '../../../assets/images/warehouse.png'
+import warehouseAdd from '../../../assets/images/warehouse-add.png'
+import warehouseEdit from '../../../assets/images/warehouse-edit.png'
+import inventory from '../../../assets/images/inventory.png'
+import inventoryAdd from '../../../assets/images/inventory-add.png'
+import inventoryEdit from '../../../assets/images/inventory-edit.png'
+
 
 class projectsInStock extends Component {
     render() {
@@ -27,8 +34,16 @@ class projectsInStock extends Component {
                     <p className="inStock__container--intro--description"><span>InStock</span> is a full-stack web application delivering an Inventory Management System for a Fortune 500 client. This application is new and improved, built from the ground up from a previous existing system that is not scalable and doesn't perform well under a heavy traffic load. My team and I created a time-sensitive, full-stack application with a modern front-end communicating with a robust Express back-end.</p>
                 </div>
                 <div className="inStock__images">
-                    {/* <img className="inStock__images--showcase1" src={googleMoods1} alt="googleMoods1"></img>
-                    <img className="inStock__images--showcase2" src={googleMoods2} alt="googleMoods2"></img> */}
+                    <div>
+                        <img className="inStock__images--warehouse" src={warehouse} alt="warehouse homepage"></img>
+                        <img className="inStock__images--warehouse" src={warehouseAdd} alt="warehouse add"></img>
+                        <img className="inStock__images--warehouse" src={warehouseEdit} alt="warehouse edit"></img>
+                    </div>
+                    <div>
+                        <img className="inStock__images--inventory" src={inventory} alt="inventory homepage"></img>
+                        <img className="inStock__images--inventory" src={inventoryAdd} alt="inventory add"></img>
+                        <img className="inStock__images--inventory" src={inventoryEdit} alt="inventory edit"></img>
+                    </div>
                 </div>
                 <div className="inStock__features">
                     <h2 className="inStock__features--title">special features</h2>
