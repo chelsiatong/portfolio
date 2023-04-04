a3aefaab713388c09f817c825f8632612f70fb15
chelsia tong <chelsiatong@gmail.com>
2023-04-04 12:06:08 -0400

    deleted unecessary files

--- src/components/projects/projectsSection.js
+++ src/components/projects/projectsSection.js
@@ -1,12 +1,10 @@
 import React, { Component } from 'react';
 import ProjectsDev from './projectsDev';
-// import ProjectsNav from './projectsNav';
 
 class ProjectsSection extends Component {
     render() {
         return (
             <div>
-                {/* <ProjectsNav /> */}
                 <ProjectsDev />
             </div>
         )
